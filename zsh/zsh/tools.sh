f2f434288ebee9d74e852e2566a0b92cde8602cf
elCuervo <yo@brunoaguirre.com>
2014-02-02 22:19:50 -0200

    Adds handling of services

--- zsh/zsh/tools.sh
+++ zsh/zsh/tools.sh
@@ -1,11 +1,22 @@
-start_services() {
+_handle_services() {
+  [ ! -f .services ] && return
+
+  action=$1
   services=$(sed 's/#.*//;/^\s*$/d' < .services)
 
   while read service; do
     brew services list | grep "$service" | grep "started" > /dev/null
 
     if [[ $? != 0 ]]; then
-      brew services start "$service" > /dev/null
+      brew services $action "$service" > /dev/null
     fi
   done < <(echo "$services")
+}
+
+start_services() {
+  _handle_services "start"
+}
+
+stop_services() {
+  _handle_services "stop"
 }
